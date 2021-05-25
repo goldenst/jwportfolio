e7f91f89dcf1145b23b6bcb6cd8d6eb883000bb2
goldenst <jeff@goldenstatetow.com>
2021-05-24 21:04:48 -0700

    home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import image from "../assets/pooh.jpg";
 
 export default class Home extends Component {
   render() {
     return (
-      <div>
-        <h1>Home Page</h1>
-      </div>
-    )
+      <main>
+        <img
+          src={image}
+          alt="Phoo Bear"
+          className="absolute object-cover h-full w-full"
+        />
+        <section className="relitive flex justify-center min-h-screen pt-8 ">
+          <h1 className="text-6xl text-black font-bold leading-none ">
+            Hello Im Jeff,
+          </h1>
+        </section>
+      </main>
+    );
   }
 }
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,44 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from "react";
+import sanityClient from "@sanity/client";
 
-export default class Post extends Component {
-  render() {
-    return (
-      <div>
-        <h1> Post Page</h1>
-      </div>
-    )
-  }
+export default function Post() {
+  const [postData, setPost] = useState(null);
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "post"]{
+      title,
+      slug,
+      mainImage{
+        asset->{
+          _id,
+          url
+        },
+        alt
+      }
+    }`
+      )
+      .then((data) => setPost(data))
+      .catch(console.error);
+  }, []);
+
+  return (
+    <main>
+      <section>
+        <h1>Blog</h1>
+        <h2>hh</h2>
+        <div>
+          <article>
+            <span>
+              <img />
+              <span>
+                <h3></h3>
+              </span>
+            </span>
+          </article>
+        </div>
+      </section>
+    </main>
+  );
 }
