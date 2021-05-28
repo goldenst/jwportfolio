596a8549c56102a1056bf1068e93628e23a7884d
goldenst <jeff@goldenstatetow.com>
2021-05-27 17:18:20 -0700

    projects page

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from "react";
-import sanityClient from "@sanity/client";
+import { Link } from "react-router-dom";
+import sanityClient from "../client";
 
 export default function Post() {
+
+
+
   const [postData, setPost] = useState(null);
 
   useEffect(() => {
     sanityClient
       .fetch(
-        `*[_type == "post"]{
+        `*[_type == 'post']{
       title,
       slug,
       mainImage{
@@ -24,19 +28,37 @@ export default function Post() {
   }, []);
 
   return (
-    <main>
-      <section>
-        <h1>Blog</h1>
-        <h2>hh</h2>
-        <div>
-          <article>
-            <span>
-              <img />
-              <span>
-                <h3></h3>
-              </span>
-            </span>
-          </article>
+    <main className="bg-grey-300 min-h-screen pt-12">
+      <section className="container mx-auto">
+        <h1 className=" text-5xl flex justify-center cursive">
+          Blog Post Page
+        </h1>
+        <h2 className="text-lg text-grey-500 flex justify-center mb-12">
+          Welcome to the Blog post page.
+        </h2>
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {postData &&
+            postData.map((post, index) => (
+              <article>
+                <Link to={"/post/" + post.slug.current} key={post.slug.current}>
+                  <span
+                    className="block h-30 relitive rounded shadow leading-snug bg-white border-l-8 border-blue-400"
+                    key={index}
+                  >
+                    <img
+                      src={post.mainImage.asset.url}
+                      alt={post.mainImage.alt}
+                      className="rounded-r relitive object-cover"
+                    />
+                    <span className="block relitive h-full flex justify-end items-end pr-4 pb-4">
+                      <h3 className="text-blue-800 text-lg font-bold px-3 py-4 bg-blue-200 bg-opasity-75 rounded">
+                        {post.title}
+                      </h3>
+                    </span>
+                  </span>
+                </Link>
+              </article>
+            ))}
         </div>
       </section>
     </main>
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,11 +1,83 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from "react";
+// import { Link } from "react-router-dom";
+import sanityClient from "../client  ";
 
-export default class Project extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Project Page</h1>
-      </div>
-    )
-  }
+export default function Project() {
+  
+
+  const [projectData, setProjectDate] = useState(null);
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "project"]{
+          title,
+          date,
+          place, 
+          description, 
+          projectType, 
+          link, 
+          tags 
+        }`
+      )
+      .then((data) => setProjectDate(data))
+      .catch(console.error);
+  }, []);
+
+  return (
+    <main className="bg-blue-100 min-h-screen p-12">
+      <section className="container mx-auto">
+        <h1 className="text-5xl flex justify-center  mb-1 cursive">
+          My Projects
+        </h1>
+        <h2 className="text-lg text-grey-600 flex justify-center mb-12">
+          Here are Some projects
+        </h2>
+        <section className="grid grid-cols-2 gap-8">
+          {projectData &&
+            projectData.map((project, index) => (
+              <article className="relitive rounded-lg shadow-xl bg-white p-5">
+                <h3 className="text-grey-800 text-3xl font-bold mb-2 hovrt:text-blue-700">
+                  <a
+                    href={project.link}
+                    alt={project.title}
+                    target="_blank"
+                    rel="noopener noreffer"
+                  >
+                    {project.title}
+                  </a>
+                </h3>
+                <div className="text-grey-500 text-xs spacex-4">
+                  <span>
+                    <strong className="font-bold">Finished on</strong>:{" "}
+                    {new Date(project.date).toLocaleDateString()}
+                  </span>
+                  <span>
+                    <strong className="font-bold">Company</strong>{" "}
+                    {project.place}
+                  </span>
+                  <span>
+                    <strong className="font-bold">type</strong>{" "}
+                    {project.projectType}
+                  </span>
+                  <p className="my-6 text-lg text-grey-700 leading-relaxed">
+                    {project.description}
+                  </p>
+                </div>
+                <a
+                  href={project.link}
+                  alt={project.title}
+                  target="_blank"
+                  rel="noopener noreffer"
+                  className="text-red-500 font-bold hover:underline hover:text-red-500"
+                >
+                  View the project{" "}
+                  <span role="img" aria-label="right-pointer"></span>
+                </a>
+              </article>
+            ))}
+        </section>
+      </section>
+    </main>
+  );
 }
